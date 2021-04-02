1c08a423d22219ccdbdc1b9f5261bc98ec9a245d
Reuben <rsreubens96@gmail.com>
2021-04-03 00:33:19 +0100

    Thumbs up and thumbs down added, threads change mouse pointer on hover

--- frontend/src/Jumbotron/JumbotronComponent.js
+++ frontend/src/Jumbotron/JumbotronComponent.js
@@ -10,14 +10,9 @@ const JumbotronComponent = () => {
           Pulse
         </h1>
         <p className="lead" style={{ color: "white" }}>
-          This is a simple hero unit, a simple Jumbotron-style component for
-          calling extra attention to featured content or information.
+          A public message-board to discuss anything
         </p>
         <hr className="my-2" style={{ color: "white" }} />
-        <p style={{ color: "white" }}>
-          It uses utility classes for typography and spacing to space content
-          out within the larger container.
-        </p>
         <p className="lead">
           <Button color="primary">New Thread</Button>
         </p>
--- frontend/src/Thread/Thread.js
+++ frontend/src/Thread/Thread.js
@@ -3,10 +3,26 @@ import "./Thread.scss";
 
 const Thread = (props) => {
   const { title, body } = props;
-  console.log(title, body);
+
+  const handleThumbsUpClick = (e) => {
+    console.log("Thumbs up pressed!");
+    e.stopPropagation();
+  };
+
+  const handleThumbsDownClick = (e) => {
+    console.log("Thumbs down pressed!");
+    e.stopPropagation();
+  };
+
+  const handleCommentsClick = (e) => {
+    console.log("Thumbs down pressed!");
+    e.stopPropagation();
+  };
+
   const handleClick = () => {
     console.log("bye");
   };
+
   return (
     <div className="thread" onClick={handleClick}>
       <h1 id="title">
@@ -37,7 +53,24 @@ const Thread = (props) => {
       </p>
 
       <div id="footer">
-        <p style={{ fontSize: "20px" }}> 2 comments</p>
+        <img
+          style={{ width: "30px", height: "30px", margin: "5px" }}
+          onClick={handleThumbsUpClick}
+          src="/thumbs-up-regular.svg"
+        />
+        <img
+          style={{ width: "30px", height: "30px", margin: "5px" }}
+          onClick={handleThumbsDownClick}
+          src="/thumbs-down-regular.svg"
+        />
+        <a style={{ fontSize: "20px", margin: "10px" }}> 8 Likes</a>
+        <a
+          style={{ fontSize: "20px", margin: "10px" }}
+          onClick={handleCommentsClick}
+          href="/"
+        >
+          2 Comments
+        </a>
       </div>
     </div>
   );
