99e3c113e210b0d9140696a83ce8a8ca7924fa2c
telkelani <56606982+telkelani@users.noreply.github.com>
2021-04-18 02:29:36 +0100

    Database connected. Getting data and adding data work

--- backend/Posts/postsServer.js
+++ backend/Posts/postsServer.js
@@ -1,7 +1,43 @@
 const express = require('express')
 const app = express()
+const db = require('./db/db.js')
+const Post = require('./models/Post.js')
 const PORT = 4000
 
-app.get('/', (request, response) => response.send("HELLO API"))
+//These two lines are needed to interpret response in JSON
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', async (request, response) => {
+    
+    try{
+        const Posts = await Post.find();
+        response.json(Posts)
+    }
+
+    catch(err){
+        response.json({message: err})
+    }
+})
+
+app.post('/newpost', async (request, response) => {
+    const submittedPost = request.body
+    const now = new Date()
+    const newPost = new Post(
+        {
+            title: submittedPost.title,
+            post: submittedPost.post,
+            user: submittedPost.user,
+            createdAt: now
+        })
+    
+    try{
+        const savedPost = await newPost.save()
+        response.json(savedPost)
+    }
+    catch (err) {
+        response.json({error_message: err})
+    }
+})
 
 app.listen(PORT, ()=> console.log(`Server running and listening on ${PORT}`))
