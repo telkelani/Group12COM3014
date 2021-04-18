8b6bd2876b87fe4c3d862b8ecc63ac5fb7a6740a
telkelani <56606982+telkelani@users.noreply.github.com>
2021-04-18 04:14:22 +0100

    Search by title and date and time

--- backend/Posts/routes/posts.js
+++ backend/Posts/routes/posts.js
@@ -4,6 +4,9 @@ const router = express.Router()
 const Post = require('../models/Post')
 
 
+//GET POST BY TITLE (Must type search?title=)
+
+
 //GET ALL POSTS
 router.get('/all', async (request, response) => {
     
@@ -17,16 +20,59 @@ router.get('/all', async (request, response) => {
     }
 })
 
+router.get('/search', async(request, response) => {
+    const query = request.query
+        if (query.start_date && query.end_date){
+            const startDate = new Date(query.start_date)
+            const endDate = new Date(query.end_date)
+            Post.find({createdAt: {
+                $gte: startDate,
+                $lt: endDate
+            }}, function(_, results) {
+                if (!results.length){
+                    response.send("Articles Not Found") 
+                }
+                else{
+                    response.json(results)
+                }
+                
+
+            })
+
+        }
+
+        else {
+            //GET POSTS BY TITLE (Like not exact match)
+            
+            const titleQuery = query.title
+            console.log(titleQuery)
+            Post.find({"title": new RegExp(titleQuery,'i')},
+            function(_, results) {
+                
+                if (!results.length){
+                    response.send("Title Not Found")
+                }
+                else{
+                    response.json(results)
+                }
+            })
+    
+            
+            
+            }
+        
+})
+
 //GET BACK SPECIFIC POST
 router.get('/:id', async (request,response) => {
     try {
-        const post = await Post.findById(request.params.id) //ADD await as this may take some time to come from database
-        response.json(post)
+        const post = await Post.findById(request.params.id)
+        response.json(post) //ADD await as this may take some time to come from database
     }
     catch(err){
         response.json({message: err})
     }
-})
+    })
 
 //GET POST BY USER
 router.get('/user/:userid', async (request, response) => {
@@ -40,6 +86,18 @@ router.get('/user/:userid', async (request, response) => {
 })
 
 
+//DELETE POST
+router.delete('/:id', async (request, response) => {
+try {
+    const removedPost = await Post.remove({_id: request.params.id})
+    response.json(removedPost)
+}
+catch (err){
+    response.json({message:err})
+}
+})
+
+//SUBMIT POST
 router.post('/newpost', async (request, response) => {
     const submittedPost = request.body
     const now = new Date()
